2d40f966d48dba7e24e3625486de3223f89f2590
Karan <karan@p3infotech.in>
2020-02-13 13:04:30 +0530

    Added a drawerNavigation after task add screen

--- src/components/AppNavigator.js
+++ src/components/AppNavigator.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import TodoApp from '../TodoApp';
+import About from '../components/About';
 import TodoDetails from '../components/TodoDetails';
 
 import { createStackNavigator } from 'react-navigation-stack';
 import { connect } from 'react-redux';
 import { addNavigationHelpers, createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
 // let TdDetails = connect()(TodoDetails);
 // let TdApp = connect()(TodoApp);
 
+let DrawerNavigation = createDrawerNavigator(
+    {
+        todoDetails: TodoDetails,
+        about: About,
+    }
+);
+
 let AppNavigator = createStackNavigator(
     {
         home: TodoApp, // TdApp,
-        todoDetails: TodoDetails, // TdDetails,
+        todoDetails: DrawerNavigation, // TdDetails,
     }
 );
 
